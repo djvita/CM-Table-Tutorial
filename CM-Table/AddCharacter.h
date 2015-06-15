88d546a782bfc016805ba0f3ef72571b6a5a69cb
Vita V <dj.vita.09@gmail.com>
2015-06-15 12:11:26 -0500

    Added image picker but its iOS 5< will put an IOS 7 and above implementation from app coda next

--- CM-Table/AddCharacter.h
+++ CM-Table/AddCharacter.h
@@ -9,7 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AddCharacter : UIViewController
+@interface AddCharacter : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate >
 
 @property (strong, nonatomic) IBOutlet UITextField *txtFName;
 @property (strong, nonatomic) IBOutlet UITextField *txtFAlias;
--- CM-Table/AddCharacter.m
+++ CM-Table/AddCharacter.m
@@ -9,6 +9,8 @@
 
 #import "AddCharacter.h"
 #import "Declarations.h"
+#import <MobileCoreServices/MobileCoreServices.h>
+
 
 @interface AddCharacter ()
 
@@ -27,7 +29,76 @@
 }
 
 - (IBAction)btnAddImagePressed:(id)sender {
+    [self startMediaBrowserFromViewController: self
+                                usingDelegate: self];
+    
 }
+- (BOOL) startMediaBrowserFromViewController: (UIViewController*) controller
+                               usingDelegate: (id <UIImagePickerControllerDelegate,
+                                               UINavigationControllerDelegate>) delegate {
+    
+    if (([UIImagePickerController isSourceTypeAvailable:
+          UIImagePickerControllerSourceTypeSavedPhotosAlbum] == NO)
+        || (delegate == nil)
+        || (controller == nil))
+        return NO;
+    
+    UIImagePickerController *mediaUI = [[UIImagePickerController alloc] init];
+    mediaUI.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    
+    // Displays saved pictures and movies, if both are available, from the
+    // Camera Roll album.
+    mediaUI.mediaTypes =
+    [UIImagePickerController availableMediaTypesForSourceType:
+     UIImagePickerControllerSourceTypeSavedPhotosAlbum];
+    
+    // Hides the controls for moving & scaling pictures, or for
+    // trimming movies. To instead show the controls, use YES.
+    mediaUI.allowsEditing = YES;
+    
+    mediaUI.delegate = delegate;
+    
+    [controller presentModalViewController: mediaUI animated: YES];
+    return YES;
+}
+- (void) imagePickerController: (UIImagePickerController *) picker
+ didFinishPickingMediaWithInfo: (NSDictionary *) info {
+    
+    NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
+    UIImage *originalImage, *editedImage, *imageToUse;
+    
+    // Handle a still image picked from a photo album
+    if (CFStringCompare ((CFStringRef) mediaType, kUTTypeImage, 0)
+        == kCFCompareEqualTo) {
+        
+        editedImage = (UIImage *) [info objectForKey:
+                                   UIImagePickerControllerEditedImage];
+        originalImage = (UIImage *) [info objectForKey:
+                                     UIImagePickerControllerOriginalImage];
+        
+        if (editedImage) {
+            imageToUse = editedImage;
+        } else {
+            imageToUse = originalImage;
+        }
+        // Do something with imageToUse
+    }
+    
+    // Handle a movied picked from a photo album
+    if (CFStringCompare ((CFStringRef) mediaType, kUTTypeMovie, 0)
+        == kCFCompareEqualTo) {
+        
+        NSString *moviePath = [[info objectForKey:
+                                UIImagePickerControllerMediaURL] path];
+        
+        // Do something with the picked movie available at moviePath
+    }
+    
+    [[picker parentViewController] dismissModalViewControllerAnimated: YES];
+    
+}
+
+
 
 - (IBAction)btnSavePressed:(id)sender {
 }
